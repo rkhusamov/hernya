783dcd158d42913228701aa3a4c56da323ae2e8c
sanerilla <sanerilla53@gmail.com>
2017-07-04 23:55:57 +0300

    Add files via upload

--- low_dash and camelCase.py
+++ low_dash and camelCase.py
@@ -1,6 +1,7 @@
 import re
 
 def camel_to_lowdash(word):
+    word=word.group(0)
     res=''+word[0].lower()
     for letter in word[1:]:
         if letter.isupper():
@@ -9,9 +10,7 @@ def camel_to_lowdash(word):
             res+=letter
     return res
 
-
-def remove_camel(string):
+def remove_camel(text_string):
     camel=re.compile(r"([a-z]+([A-Z][a-z]+)+)|([A-Z][a-z]+){2,}")
-    
-    return re.sub(camel, camel_to_lowdash, string)
+    return re.sub(camel, camel_to_lowdash, text_string)
 
